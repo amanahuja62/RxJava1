fcbd5f9bf3073d17478624bebcd9a49a04f7896f
Aman Ahuja <aman.ahuja680@gmail.com>
2021-11-20 00:54:44 +0530

    committing some files

---
+++ app/src/main/java/com/example/rxjava/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.rx1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import com.example.rxjava.R
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.annotations.NonNull
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.observers.DisposableObserver
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    private val TAG = "hello"
+    lateinit var textView: TextView
+    lateinit var observable : Observable<String>
+    var disposable: Disposable? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.textView)
+        observable = Observable.just("Hello Rx Java","Gaurav","Pranav","Aman")
+        observable.subscribeOn(Schedulers.io())
+        observable.observeOn(AndroidSchedulers.mainThread())
+        observable.subscribe(object : Observer<String>{
+            override fun onSubscribe(d: Disposable?) {
+                disposable = d
+            }
+
+            override fun onNext(t: String?) {
+                textView.setText(t)
+                if(t != null)
+                Log.i(TAG,t)
+                Thread.sleep(1000)
+            }
+
+            override fun onError(e: Throwable?) {
+                Log.i(TAG,e.toString())
+            }
+
+            override fun onComplete() {
+                Log.i(TAG,"Task has been completed")
+            }
+
+        })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposable?.dispose()
+    }
+}
